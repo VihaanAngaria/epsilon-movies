0dc98befa74a5e9635f21d69c6eca5370370525b
Vihaan Angaria <vihaanangaria@gmail.com>
2025-05-11 16:32:26 +0530

    Paused redirect feature

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def watch():
 @app.route('/sponsor')
 def sponsor():
     # Redirect to sponsor (Shotify) link
-    return redirect("https://shrtlk.click/81jy8")
+#    return redirect("https://shrtlk.click/81jy8")
 
 @app.route('/afterSponsor')
 def after_sponsor():
