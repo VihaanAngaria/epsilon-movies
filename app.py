57f74f9e3d89fe6f95f3d3ef3665f2f2c5ca5e14
Vihaan Angaria <vihaanangaria@gmail.com>
2025-05-11 16:45:07 +0530

    Updated watch functionality

--- app.py
+++ app.py
@@ -6,19 +6,22 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+
+
 @app.route('/watch')
 def watch():
-    return redirect('/sponsor')
-
-@app.route('/sponsor')
-def sponsor():
-    # Redirect to sponsor (Shotify) link
-#    return redirect("https://shrtlk.click/81jy8")
-
-@app.route('/afterSponsor')
-def after_sponsor():
-    # Redirect to NetMirror after sponsor
     return redirect("https://netfree2.cc/login")
+
+# @app.route('/sponsor')
+# def sponsor():
+#     # Redirect to sponsor (Shotify) link
+#     return redirect("https://shrtlk.click/81jy8")
+
+# @app.route('/afterSponsor')
+# def after_sponsor():
+#     # Redirect to NetMirror after sponsor
+#     return redirect("https://netfree2.cc/login")
 
 if __name__ == '__main__':
     app.run(debug=True)
